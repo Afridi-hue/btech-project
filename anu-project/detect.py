56b7c91e920327ae21864d186a3c9d0826db7637
Alpha-1729 <anilakskumar1729@gmail.com>
2020-02-04 09:32:01 -0800

    added link

--- anu-project/detect.py
+++ anu-project/detect.py
@@ -1,3 +1,4 @@
-keras.models.load_model("normal/a.jpg")
-model = load_model('my_model.h5')
-model.evaluate(x,y)
+# https://stackoverflow.com/questions/50151585/how-to-use-a-saved-model-in-keras-to-predict-and-classify-an-image
+from keras.models import load_model
+model = load_model('saved_model.h5')
+model.evaluate()
