528e3b189b3629677ccf9dc039d3adf5b2befc8c
Alpha-1729 <anilakskumar1729@gmail.com>
2020-02-04 09:34:24 -0800

    added new code

--- anu-project/detect.py
+++ anu-project/detect.py
@@ -1,4 +1,15 @@
 # https://stackoverflow.com/questions/50151585/how-to-use-a-saved-model-in-keras-to-predict-and-classify-an-image
 from keras.models import load_model
 model = load_model('saved_model.h5')
-model.evaluate()
+model.evaluate()\
+
+
+"""
+from keras.preprocessing import image
+
+test_image= image.load_img(picturePath, target_size = (img_width, img_height)) 
+test_image = image.img_to_array(test_image)
+test_image = numpy.expand_dims(test_image, axis = 0)
+test_image = test_image.reshape(img_width, img_height)
+result = model.predict(test_image)   
+"""
