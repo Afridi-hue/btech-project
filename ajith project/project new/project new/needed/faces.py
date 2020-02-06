cdb782690ecc85c3d91631681887849e15168121
Alpha-1729 <anilakskumar1729@gmail.com>
2020-02-06 02:58:11 -0800

    added files

--- ajith project/project new/project new/needed/faces.py
+++ ajith project/project new/project new/needed/faces.py
@@ -3,55 +3,52 @@ import cv2
 import pickle
 
 # for face detection
-face_cascade = cv2.CascadeClassifier('fdface.xml')
+# face_cascade = cv2.CascadeClassifier('fdface.xml')
 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("./recognizers/face-trainner.yml")
+recognizer.read("../trainner.yml")
 
 labels = {"person_name": 1}
-with open("pickles/face-labels.pickle", 'rb') as f:
+with open("../labels.pickle", 'rb') as f:
 	og_labels = pickle.load(f)
 	labels = {v:k for k,v in og_labels.items()}
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for (x, y, w, h) in faces:
-    	#print(x,y,w,h)
-    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-    	roi_color = frame[y:y+h, x:x+w]
+frame=cv2.imread("sample.jpg")
+gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+height_1, width_1, channels = frame.shape
+roi_gray = gray[0:height_1, 0:width_1] #(ycord_start, ycord_end)
+roi_color = frame[0:height_1, 0:width_1]
 
-    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
-    	id_, conf = recognizer.predict(roi_gray)
-    	if conf>=4 and conf <= 85:
-    		#print(5: #id_)
-    		#print(labels[id_])
-    		font = cv2.FONT_HERSHEY_SIMPLEX
-    		name = labels[id_]
-    		color = (255, 255, 255)
-    		stroke = 2
-    		cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
 
-    	img_item = "7.png"
-    	cv2.imwrite(img_item, roi_color)
+# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+id_, conf = recognizer.predict(roi_gray)
+print(conf,labels[id_])
+if conf>=4 and conf <= 85:
+    
+    #print(5: #id_)
+    #print(labels[id_])
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    name = labels[id_]
+    print(name)
+    color = (255, 255, 255)
+    stroke = 2
+    #cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
 
-    	color = (255,255, 0)
-    	stroke = 2
-    	end_cord_x = x+w+40
-    	end_cord_y = y+h+40
-    	cv2.rectangle(frame, (x-40, y-40), (end_cord_x, end_cord_y), color, stroke)
-    	#subitems = smile_cascade.detectMultiScale(roi_gray)
-    	#for (ex,ey,ew,eh) in subitems:
-    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+    img_item = "7.png"
+    cv2.imwrite(img_item, roi_color)
 
-cap.release()
-cv2.destroyAllWindows()
+##    color = (255,255, 0)
+##    stroke = 2
+##    end_cord_x = x+w+40
+##    end_cord_y = y+h+40
+##    cv2.rectangle(frame, (x-40, y-40), (end_cord_x, end_cord_y), color, stroke)
+    #subitems = smile_cascade.detectMultiScale(roi_gray)
+    #for (ex,ey,ew,eh) in subitems:
+    #	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+# Display the resulting frame
+cv2.imshow('frame',frame)
+if cv2.waitKey(0):
+    cv2.destroyAllWindows()
+
